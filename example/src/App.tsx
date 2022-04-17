dec26ee66a5b6685cbb460a9ce6b17d5653f48be
P-Chan <hello@0x50.io>
2022-04-17 22:27:39 +0900

    chore: add example

---
+++ example/src/App.tsx
@@ -0,0 +1,32 @@
+import { useCallback, useState } from 'react'
+
+import { useAdobeFonts } from '../../dist'
+
+const App = () => {
+  const [isLoading, setIsLoading] = useState(false)
+  const [isActive, setIsActive] = useState(false)
+
+  const onLoading = useCallback(() => {
+    setIsLoading(true)
+  }, [])
+
+  const onActive = useCallback(() => {
+    setIsLoading(false)
+    setIsActive(true)
+  }, [])
+
+  useAdobeFonts({
+    kitId: '',
+    onLoading,
+    onActive,
+  })
+
+  return (
+    <div>
+      <div>isLoading: {isLoading ? 'true' : 'false'}</div>
+      <div>isActive: {isActive ? 'true' : 'false'}</div>
+    </div>
+  )
+}
+
+export default App
